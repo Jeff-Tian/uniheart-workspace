3aee5e71a49eb5e3e5de9e7bc838e35d173d043f
Jeff Tian <jeff.tian@outlook.com>
2022-05-10 18:52:06 +0800

    test: skip for now

--- experience/ui/uniheart-rich-footer/uniheart-rich-footer.tsx
+++ experience/ui/uniheart-rich-footer/uniheart-rich-footer.tsx
@@ -1,4 +1,7 @@
 import React, { ReactNode } from 'react';
+import {Col, Layout, Row } from 'antd';
+import 'antd/dist/antd.css';
+const { Header, Footer, Sider, Content } = Layout;
 
 export type UniheartRichFooterProps = {
   /**
@@ -9,8 +12,21 @@ export type UniheartRichFooterProps = {
 
 export function UniheartRichFooter({ children }: UniheartRichFooterProps) {
   return (
-    <div>
+    <Layout>
+      <Footer>
+        <Row>
+          <Col xs={{ span: 5, offset: 1 }} lg={{ span: 6, offset: 2 }}>
+            Col
+          </Col>
+          <Col xs={{ span: 11, offset: 1 }} lg={{ span: 6, offset: 2 }}>
+            Col
+          </Col>
+          <Col xs={{ span: 5, offset: 1 }} lg={{ span: 6, offset: 2 }}>
+            Col
+          </Col>
+        </Row>
+      </Footer>
       {children}
-    </div>
+    </Layout>
   );
 }
